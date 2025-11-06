de13428831cef6d2dc7edaa896de98cd8bdf09bf
sitangruan <sitangruan@gmail.com>
2025-11-05 22:17:38 -0500

    Add Sitang name

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.less'
 })
 export class App {
-  protected readonly title = signal('angular2');
+  protected readonly title = signal('Sitang\'s angular2');
 }
