2a1dc20df63ccac80d008467fe9896b0177e4e99
sitangruan <sitangruan@gmail.com>
2025-11-17 21:32:24 -0500

    Implement default route redirect and active route class

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,24 @@
 import { Routes } from "@angular/router";
 import { routeList } from "../common/constants";
+import { NavigationLink } from "../modals/NavigationLink";
 
-export const routes: Routes = routeList.map(route => ({
-  path: route.route,
-  title: route.displayName,
-  loadComponent: route.loadComponent,
-}));
+const populateRoutes = (links: NavigationLink[]): Routes => {
+
+  const routes: Routes = [];
+  const defaulLink = links.find(link => link.isDefaultLink);
+  if (defaulLink) {
+    routes.push({ path: '', redirectTo: defaulLink.route, pathMatch: 'full' });
+  }
+
+  links.forEach(link => {
+    routes.push({
+      path: link.route,
+      title: link.displayName,
+      loadComponent: link.loadComponent,
+    });
+  });
+
+  return routes;
+}
+
+export const routes: Routes = populateRoutes(routeList);
--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { routeList } from '../../common/constants';
+import { RouterLinkActive } from "@angular/router";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLinkActive, RouterModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
